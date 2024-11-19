61a256f55470aa8f855e0acbcdcf813ba50fcc03
Zexian Huang <k305261558@icloud.com>
2024-11-19 15:08:42 +1100

    update torch template

--- train/trainer.py
+++ train/trainer.py
@@ -1,144 +1,201 @@
 import yaml
+import logging
 import torch
 
-from tqdm import tqdm
 from pathlib import Path
 from typing import Union, Optional, Callable, Dict
+from tqdm import tqdm
 
-from torch_geometric.loader import DataLoader
-from torch_geometric.nn import MessagePassing
-
-from torch.utils.tensorboard import SummaryWriter
-from utils.torch_tools import EarlyStopping
-from torchmetrics import Metric
-from torchmetrics.classification import BinaryAccuracy, BinaryJaccardIndex
 from torchinfo import summary
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from torchmetrics.classification import Precision, Recall, F1Score, JaccardIndex
+
+from utils.utils import EarlyStopping
 
 
 class Trainer:
-    def __init__(self, cfg:Union[str, Path, dict]):
+    def __init__(self, cfg:Union[str, Path, Dict]):
         if isinstance(cfg, str or Path):
             with open(cfg, 'r') as f:
                 self.cfg = yaml.safe_load(f)    
-        elif isinstance(cfg, dict):
+        elif isinstance(cfg, Dict):
             self.cfg = cfg
+        else:
+            raise ValueError("trainer cfg must be a string, Path, or dictionary.")
             
         self.epoch = self.cfg['epoch']
         self.path = self.cfg['path']
         self.patience = self.cfg['patience']
         self.lr = self.cfg['lr']
         self.w_decay = self.cfg['w_decay']
-        self.device = self.cfg['device']
-            
-    def fit(self, 
-            model: Union[torch.nn.Module, MessagePassing],  
+        
+        self.metrics = {
+            'Precision': Precision(task='binary', threshold=0.5).to(self.cfg['device']),
+            'Recall': Recall(task='binary', threshold=0.5).to(self.cfg['device']),
+            'F1Score': F1Score(task='binary', threshold=0.5).to(self.cfg['device']),
+            'mIoU': JaccardIndex(task='binary', threshold=0.5).to(self.cfg['device'])
+        }
+
+    def fit(self, model: torch.nn.Module, 
             criterion: Optional[Callable]=None,  
             optimizer: Optional[torch.optim.Optimizer]=None, 
             train_loader: Optional[DataLoader]=None,
             val_loader: Optional[DataLoader]=None,
             ckpt: Union[str, Path, None]=None,
-            save_period: int=10,
-            writer: Optional[object]=None, 
-            early_stop: bool=False):
-        summary(model, depth=100)
+            save_period: int=1):
+        summary(model)
+        model = model.to(self.cfg['device'])
         
-        model.to(self.device)  
-        model.load_state_dict(self._load_ckpt(ckpt)['params']) if ckpt else model  
+        if ckpt:
+            model.load_state_dict(self._load_ckpt(ckpt)['params'])
         
-        criterion = torch.nn.CrossEntropyLoss() if criterion == None else criterion
+        criterion = criterion or torch.nn.CrossEntropyLoss()
+
+        optimizer = optimizer or torch.optim.AdamW(
+            model.parameters(),
+            lr=self.lr,
+            weight_decay=self.w_decay
+        )
         
-        if not optimizer: optimizer = torch.optim.Adam(model.parameters(), 
-                                                       lr=self.lr, 
-                                                       weight_decay=self.w_decay) 
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer,
+            T_0=self.epoch,
+            T_mult=1,
+            eta_min=1e-6,
+            verbose=False
+        )
         
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 
-                                                                            T_0=self.epoch, 
-                                                                            T_mult=1,
-                                                                            eta_min=1e-6)
-        
-        writer = SummaryWriter(log_dir=f'{self.path}/runs') if writer == None else writer
-        
-        if early_stop: early_stopping = EarlyStopping(path=self.path, patience=self.patience)
+        writer = SummaryWriter(log_dir=f'{self.path}/runs')
+        early_stopping = EarlyStopping(path=self.path, patience=self.patience)
          
-        for ep in tqdm(range(1, self.epoch+1)):
-            t_ls = self._fit_impl(model, optimizer, criterion, train_loader)
-            writer.add_scalar('Loss/train', t_ls, ep)
+        for ep in tqdm(range(1, self.epoch + 1)):
+            t_loss, t_mcs = self._fit_impl(model, optimizer, criterion, train_loader)
+            writer.add_scalar('Loss/train', t_loss, ep)
+            print('Loss/train', f"{t_loss:.2f}", end=' -- ')
             
+            for mc_name, mc_value in t_mcs.items():
+                writer.add_scalar(f'{mc_name}/train', mc_value, ep)
+                print(mc_name+'/train', f"{mc_value * 100:.2f}", end=' -- ')
+
             # Adjust learning rate 
             lr_scheduler.step()
             writer.add_scalar('LRate/train', lr_scheduler.get_last_lr()[0], ep)
-            
-            if ep % save_period == 0: # save model at every n epoch
-                if val_loader:
-                    v_ls = self._val_impl(model, criterion, val_loader) 
-                    writer.add_scalar('Loss/val', v_ls, ep)
+    
+            if val_loader:
+                v_loss, v_mcs = self._val_impl(model, criterion, val_loader) 
+                writer.add_scalar('Loss/val', v_loss, ep)
+                print('Loss/val', f"{v_loss:.2f}", end=' -- ')
+                
+                for mc_name, mc_value in v_mcs.items():
+                    writer.add_scalar(f'{mc_name}/val', mc_value, ep)
+                    print(mc_name+'/val', f"{mc_value * 100:.2f}", end=' -- ')
                     
-                    if early_stop:
-                        early_stopping(v_ls.item(), model, optimizer, ep, lr_scheduler.get_last_lr()) # accpet float type
-                        if early_stopping.early_stop: break
-            
+                if ep % save_period == 0: # save model at every n epoch  
+                    early_stopping(v_loss, model, optimizer, ep, lr_scheduler.get_last_lr())
+                    
+                    if early_stopping.early_stop: 
+                        break
+
     def _fit_impl(self, model, optimizer, criterion, dataloader):
         model.train()
         ls = 0.0
-        for _, data in enumerate(dataloader):
+        self._reset_metrics()
+        
+        for step, data in enumerate(dataloader):
+            label, input = data['label'].to(self.cfg['device']), data['input'].to(self.cfg['device'])
             optimizer.zero_grad()         # Clear gradients
-            data.to(self.device)
-            logits = model(data) 
-            loss = criterion(logits, data['y'])   # Compute gradients
+            out = model(input)
+            loss = criterion(out, label)   # Compute gradients
             loss.backward()               # Backward pass 
             optimizer.step()              # Update model parameters                                                       
-            # Loss dim reduction="mean"
-            ls += len(data) * loss.detach().clone()  
-        return ls / len(dataloader.dataset)
+            
+            ls += loss.item() * out.size(0)
+            self._update_metrics(out, label)
+            
+        per_sample_loss = ls / len(dataloader.dataset)
+        mcs = dict(zip(list(self.metrics.keys()), self._compute_metrics())) 
+        return per_sample_loss, mcs
     
     def _val_impl(self, model, criterion, dataloader):
         model.eval()
         ls = 0.0
-        for _, data in enumerate(dataloader):
-            data.to(self.device)
-            logits = model(data) 
-            loss = criterion(logits, data['y'])   
-            ls += len(data) * loss.detach().clone()
-        return ls / len(dataloader.dataset)
-    
-    def eval(self, 
-             model: Union[torch.nn.Module, MessagePassing], 
+        self._reset_metrics()
+        
+        with torch.no_grad():
+            for step, data in enumerate(dataloader):
+                label, input = data['label'].to(self.cfg['device']), data['input'].to(self.cfg['device'])
+                out = model(input)
+                loss = criterion(out, label)   
+                ls += loss.item() * out.size(0)
+                self._update_metrics(out, label)
+            
+        per_sample_loss = ls / len(dataloader.dataset)
+        mcs = dict(zip(list(self.metrics.keys()), self._compute_metrics())) 
+        return per_sample_loss, mcs
+
+    def eval(self, model: Union[torch.nn.Module], 
+             criterion: Optional[Callable]=None,  
              dataloader: Optional[DataLoader]=None, 
-             metric: Optional[Dict[str, Metric]]=None,
              ckpt: Union[str, Path, None]=None,
              verbose: bool=False):
-        model.load_state_dict(self._load_ckpt(ckpt, self.device)['model_state_dict']) if ckpt else model  
-        model.to(self.device)
+        summary(model)
+        model = model.to(self.cfg['device'])
         
-        if metric is None:
-            metric = {'OA': BinaryAccuracy(), 
-                      'mIoU': BinaryJaccardIndex()}
+        if ckpt:
+            model.load_state_dict(self._load_ckpt(ckpt)['params'])
             
-        for name, cm in metric.items(): 
-            cm.to(self.device)
+        criterion = criterion or torch.nn.CrossEntropyLoss()
 
-        metric = self._eval_impl(model, metric, dataloader)
+        e_loss, e_mcs = self._eval_impl(model, criterion, dataloader) 
         
         if verbose:
-            for name, cm in metric.items(): print(f"{name}: {cm.compute().cpu().numpy().tolist()}")    
-        return metric
-    
-    def _eval_impl(self, model, metric, dataloader):
+            print('Loss/eval', f"{e_loss:.2f}", end=' -- ')
+            
+            for mc_name, mc_value in e_mcs.items():
+                print(mc_name+'/eval', f"{mc_value * 100:.2f}", end=' -- ')
+
+    def _eval_impl(self, model, criterion, dataloader):
         model.eval()
-        for data in dataloader:
-            data.to(self.device)
-            logits = model(data) 
-            for name, cm in metric.items():
-                cm.update(logits, data['y'])
-        return metric 
+        ls = 0.0
+        self._reset_metrics()
+        
+        with torch.no_grad():
+            for step, data in enumerate(dataloader):
+                label, input = data['label'].to(self.cfg['device']), data['input'].to(self.cfg['device'])
+                out = model(input)
+                loss = criterion(out, label)   
+                ls += loss.item() * out.size(0)
+                self._update_metrics(out, label)
+            
+        per_sample_loss = ls / len(dataloader.dataset)
+        mcs = dict(zip(list(self.metrics.keys()), self._compute_metrics())) 
+        return per_sample_loss, mcs
     
     def load_weights(self, model: torch.nn.Module, ckpt: Union[str, Path]):
         if isinstance(ckpt, str): ckpt = Path(ckpt)
-        model.load_state_dict(self._load_ckpt(ckpt, self.device)['model_state_dict']) 
+        model.load_state_dict(self._load_ckpt(ckpt)['params']) 
         model.eval()
-        model.to(self.device)
+        model.to(self.cfg['device'])
         return model
+        
+    def _save_ckpt(self, model, ckpt_name):
+        ckpt_path = Path(self.path) / 'ckpt'
+        ckpt_path.mkdir(parents=True, exist_ok=True)
+        torch.save({'params': model.state_dict()}, ckpt_path / ckpt_name)
+        logging.info(f'Model checkpoint saved at {ckpt_path / ckpt_name}')
+        
+    def _load_ckpt(self, ckpt_name):
+        ckpt_path = Path(self.path) / 'ckpt' / ckpt_name 
+        return torch.load(ckpt_path) # {'params': Tensor}
     
-    def _load_ckpt(self, ckpt_name, device):
-        path = Path(self.path) / 'ckpt'
-        return torch.load(path.joinpath(ckpt_name), map_location=device)
+    def _update_metrics(self, pred, target):
+        for metric in self.metrics.values():
+            metric.update(pred, target.long())
+            
+    def _compute_metrics(self):
+        return [metric.compute().item() for metric in self.metrics.values()]
+        
+    def _reset_metrics(self):
+        for metric in self.metrics.values():
+            metric.reset()
