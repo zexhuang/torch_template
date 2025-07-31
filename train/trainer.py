7116bd9eceb764607b0f9876dc7fd00b115918d3
Zexian Huang <k305261558@icloud.com>
2025-08-01 06:42:19 +1000

    Update trainer.py

--- train/trainer.py
+++ train/trainer.py
@@ -1,212 +1,162 @@
 import yaml
 import logging
-import torch
-
+import random
+import numpy as np
 from pathlib import Path
 from typing import Union, Optional, Callable, Dict
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import MessagePassing
+from torchmetrics import Metric
+from torchmetrics.classification import BinaryAccuracy, BinaryJaccardIndex
+from torchinfo import summary
 from tqdm import tqdm
 
-from torchinfo import summary
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DataLoader
-from torchmetrics.classification import Precision, Recall, F1Score, JaccardIndex
 
-from utils.utils import EarlyStopping
-
-
-class Trainer:
-    def __init__(self, cfg:Union[str, Path, Dict]):
-        if isinstance(cfg, str or Path):
+class BaseTrainer:
+    def __init__(self, cfg: Union[str, Path, dict]):
+        if isinstance(cfg, (str, Path)):
             with open(cfg, 'r') as f:
-                self.cfg = yaml.safe_load(f)    
-        elif isinstance(cfg, Dict):
+                self.cfg = yaml.safe_load(f)
+        elif isinstance(cfg, dict):
             self.cfg = cfg
         else:
-            raise ValueError("trainer cfg must be a string, Path, or dictionary.")
-            
+            raise ValueError("Config must be a string, Path, or dictionary.")
+
         self.epoch = self.cfg['epoch']
         self.path = self.cfg['path']
         self.patience = self.cfg['patience']
         self.lr = self.cfg['lr']
         self.w_decay = self.cfg['w_decay']
-        
-        self.metrics = {
-            'Precision': Precision(task='binary', threshold=0.5).to(self.cfg['device']),
-            'Recall': Recall(task='binary', threshold=0.5).to(self.cfg['device']),
-            'F1Score': F1Score(task='binary', threshold=0.5).to(self.cfg['device']),
-            'mIoU': JaccardIndex(task='binary', threshold=0.5).to(self.cfg['device'])
-        }
-        
+        self.num_cls = self.cfg['out_channels']
+        self.device = self.cfg['device']
+
         self.set_seed()
 
-    def fit(self, model: torch.nn.Module, 
-            criterion: Optional[Callable]=None,  
-            optimizer: Optional[torch.optim.Optimizer]=None, 
-            train_loader: Optional[DataLoader]=None,
-            val_loader: Optional[DataLoader]=None,
-            ckpt: Union[str, Path, None]=None,
-            save_period: int=1):
-        summary(model)
-        model = model.to(self.cfg['device'])
-        
-        if ckpt:
-            model.load_state_dict(self._load_ckpt(ckpt)['model_state_dict'], strict=False)
-        
-        criterion = criterion or torch.nn.CrossEntropyLoss()
-
-        optimizer = optimizer or torch.optim.AdamW(
-            model.parameters(),
-            lr=self.lr,
-            weight_decay=self.w_decay
-        )
-        
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-            optimizer,
-            T_0=self.epoch,
-            T_mult=1,
-            eta_min=1e-6,
-            verbose=False
-        )
-        
-        writer = SummaryWriter(log_dir=f'{self.path}/runs')
-        early_stopping = EarlyStopping(path=self.path, patience=self.patience)
-         
-        for ep in tqdm(range(1, self.epoch + 1)):
-            t_loss, t_mcs = self._fit_impl(model, optimizer, criterion, train_loader)
-            writer.add_scalar('Loss/train', t_loss, ep)
-            print('Loss/train', f"{t_loss:.2f}", end=' -- ')
-            
-            for mc_name, mc_value in t_mcs.items():
-                writer.add_scalar(f'{mc_name}/train', mc_value, ep)
-                print(mc_name+'/train', f"{mc_value * 100:.2f}", end=' -- ')
-
-            # Adjust learning rate 
-            lr_scheduler.step()
-            writer.add_scalar('LRate/train', lr_scheduler.get_last_lr()[0], ep)
-    
-            if val_loader:
-                v_loss, v_mcs = self._val_impl(model, criterion, val_loader) 
-                writer.add_scalar('Loss/val', v_loss, ep)
-                print('Loss/val', f"{v_loss:.2f}", end=' -- ')
-                
-                for mc_name, mc_value in v_mcs.items():
-                    writer.add_scalar(f'{mc_name}/val', mc_value, ep)
-                    print(mc_name+'/val', f"{mc_value * 100:.2f}", end=' -- ')
-                    
-                if ep % save_period == 0: # save model at every n epoch  
-                    early_stopping(v_loss, model, optimizer, ep, lr_scheduler.get_last_lr())
-                    
-                    if early_stopping.early_stop: 
-                        break
-
-    def _fit_impl(self, model, optimizer, criterion, dataloader):
-        model.train()
-        ls = 0.0
-        self._reset_metrics()
-        
-        for step, data in enumerate(dataloader):
-            label, input = data['label'].to(self.cfg['device']), data['input'].to(self.cfg['device'])
-            optimizer.zero_grad()         # Clear gradients
-            out = model(input)
-            loss = criterion(out, label)  # Compute gradients
-            loss.backward()               # Backward pass 
-            optimizer.step()              # Update model parameters                                                       
-            
-            ls += loss.item() * out.size(0)
-            self._update_metrics(out, label)
-            
-        per_sample_loss = ls / len(dataloader.dataset)
-        mcs = dict(zip(list(self.metrics.keys()), self._compute_metrics())) 
-        return per_sample_loss, mcs
-    
-    def _val_impl(self, model, criterion, dataloader):
-        model.eval()
-        ls = 0.0
-        self._reset_metrics()
-        
-        with torch.no_grad():
-            for step, data in enumerate(dataloader):
-                label, input = data['label'].to(self.cfg['device']), data['input'].to(self.cfg['device'])
-                out = model(input)
-                loss = criterion(out, label)   
-                ls += loss.item() * out.size(0)
-                self._update_metrics(out, label)
-            
-        per_sample_loss = ls / len(dataloader.dataset)
-        mcs = dict(zip(list(self.metrics.keys()), self._compute_metrics())) 
-        return per_sample_loss, mcs
-
-    def eval(self, model: Union[torch.nn.Module], 
-             criterion: Optional[Callable]=None,  
-             dataloader: Optional[DataLoader]=None, 
-             ckpt: Union[str, Path, None]=None,
-             verbose: bool=False):
-        model = model.to(self.cfg['device'])
-        
-        if ckpt:
-            model.load_state_dict(self._load_ckpt(ckpt)['model_state_dict'])
-            
-        criterion = criterion or torch.nn.CrossEntropyLoss()
-
-        e_loss, e_mcs = self._eval_impl(model, criterion, dataloader) 
-        
-        if verbose:
-            summary(model)
-            
-            print('Loss/eval', f"{e_loss:.2f}", end=' -- ')
-            for mc_name, mc_value in e_mcs.items():
-                print(mc_name+'/eval', f"{mc_value * 100:.2f}", end=' -- ')
-                
-        return e_mcs
-
-    def _eval_impl(self, model, criterion, dataloader):
-        model.eval()
-        ls = 0.0
-        self._reset_metrics()
-        
-        with torch.no_grad():
-            for step, data in enumerate(dataloader):
-                label, input = data['label'].to(self.cfg['device']), data['input'].to(self.cfg['device'])
-                out = model(input)
-                loss = criterion(out, label)   
-                ls += loss.item() * out.size(0)
-                self._update_metrics(out, label)
-            
-        per_sample_loss = ls / len(dataloader.dataset)
-        mcs = dict(zip(list(self.metrics.keys()), self._compute_metrics())) 
-        return per_sample_loss, mcs
-    
-    def load_weights(self, model: torch.nn.Module, ckpt: Union[str, Path]):
-        if isinstance(ckpt, str): ckpt = Path(ckpt)
-        model.load_state_dict(self._load_ckpt(ckpt)['model_state_dict'], strict=False) 
-        model.eval()
-        model.to(self.cfg['device'])
-        return model
-    
-    def set_seed(self, seed = 3407):
+    def set_seed(self, seed=42):
+        random.seed(seed)
+        np.random.seed(seed)
         torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)  # For multi-GPU setups
-        torch.backends.cudnn.deterministic = True  # Ensures deterministic behavior
-        torch.backends.cudnn.benchmark = False  # Disables optimization for reproducibility
-        
+
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    def load_weights(self, model: torch.nn.Module, ckpt: Union[str, Path]):
+        ckpt = Path(ckpt) if isinstance(ckpt, str) else ckpt
+        model.load_state_dict(self._load_ckpt(ckpt, self.device)['params'])
+        model.eval().to(self.device)
+        return model
+
     def _save_ckpt(self, model, ckpt_name):
-        ckpt_path = Path(self.path) / 'ckpt'
-        ckpt_path.mkdir(parents=True, exist_ok=True)
-        torch.save({'model_state_dict': model.state_dict()}, ckpt_path / ckpt_name)
-        logging.info(f'Model checkpoint saved at {ckpt_path / ckpt_name}')
-        
-    def _load_ckpt(self, ckpt_name):
-        ckpt_path = Path(self.path) / 'ckpt' / ckpt_name 
-        return torch.load(ckpt_path)
-    
-    def _update_metrics(self, pred, target):
-        for metric in self.metrics.values():
-            metric.update(pred, target.long())
-            
-    def _compute_metrics(self):
-        return [metric.compute().item() for metric in self.metrics.values()]
-        
-    def _reset_metrics(self):
-        for metric in self.metrics.values():
-            metric.reset()
+        ckpt_dir = Path(self.path) / 'ckpt'
+        ckpt_dir.mkdir(parents=True, exist_ok=True)
+        torch.save({'params': model.state_dict()}, ckpt_dir / ckpt_name)
+        logging.info(f'Model checkpoint saved: {ckpt_name}')
+
+    def _load_ckpt(self, ckpt_name, device):
+        ckpt_path = Path(self.path) / 'ckpt' / ckpt_name
+        return torch.load(ckpt_path, map_location=device, weights_only=True)
+
+
+class Trainer(BaseTrainer):
+    def fit(self, 
+            model: Union[torch.nn.Module, MessagePassing],
+            criterion: Optional[Callable] = None,
+            train_loader: Optional[DataLoader] = None,
+            val_loader: Optional[DataLoader] = None,
+            ckpt: Union[str, Path, None] = None,
+            save_period: int = 10):
+
+        summary(model, depth=100)
+        if ckpt:
+            model.load_state_dict(self._load_ckpt(ckpt, self.device)['params'])
+
+        criterion = criterion or torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.AdamW(model.parameters(), lr=self.lr, weight_decay=self.w_decay)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.epoch, eta_min=1e-5)
+
+        self.writer = SummaryWriter(log_dir=f'{self.path}/runs')
+        early_stopping = EarlyStopping(path=self.path, patience=self.patience)
+
+        for ep in tqdm(range(1, self.epoch + 1)):
+            train_loss = self._train_one_epoch(model, optimizer, criterion, train_loader)
+            self.writer.add_scalar('Loss/train', train_loss, ep)
+            self.writer.add_scalar('LRate/train', lr_scheduler.get_last_lr()[0], ep)
+            lr_scheduler.step()
+
+            if ep % save_period == 0:
+                if val_loader:
+                    val_loss = self._evaluate(model, criterion, val_loader)
+                    self.writer.add_scalar('Loss/val', val_loss, ep)
+                    early_stopping(val_loss, model, optimizer, ep, lr_scheduler.get_last_lr())
+                    if early_stopping.early_stop:
+                        logging.info(f"Early stopping at epoch {ep}.")
+                        break
+                else:
+                    self._save_ckpt(model, ckpt_name=f'epoch{ep}')
+
+    def _train_one_epoch(self, model, optimizer, criterion, dataloader):
+        model.train().to(self.device)
+        total_loss = 0.0
+
+        for data in dataloader:
+            data = data.to(self.device)
+            optimizer.zero_grad()
+            out = model(data)
+            loss = criterion(out['y'], data['y'])
+            loss.backward()
+            optimizer.step()
+            total_loss += len(data) * loss.item()
+
+        return total_loss / len(dataloader.dataset)
+
+    def _evaluate(self, model, criterion, dataloader):
+        model.eval().to('cpu')
+        total_loss = 0.0
+
+        with torch.no_grad():
+            for data in dataloader:
+                data = data.to('cpu')
+                out = model(data)
+                loss = criterion(out['y'], data['y'])
+                total_loss += len(data) * loss.item()
+
+        return total_loss / len(dataloader.dataset)
+
+    def eval(self, 
+             model: Union[torch.nn.Module, MessagePassing],
+             dataloader: DataLoader,
+             metric: Optional[Dict[str, Metric]] = None,
+             ckpt: Union[str, Path, None] = None,
+             verbose: bool = False):
+
+        if ckpt:
+            model.load_state_dict(self._load_ckpt(ckpt, self.device)['params'])
+
+        metric = metric or {
+            'OA': BinaryAccuracy(),
+            'mIoU': BinaryJaccardIndex()
+        }
+
+        model.eval().to(self.device)
+        for cm in metric.values():
+            cm.to(self.device)
+
+        with torch.no_grad():
+            for data in dataloader:
+                data = data.to(self.device)
+                out = model(data)
+                for name, cm in metric.items():
+                    cm.update(out['y'], data['y'].long())
+
+        if verbose:
+            for name, cm in metric.items():
+                print(f"{name}: {cm.compute().cpu().item()}")
+
+        return metric
